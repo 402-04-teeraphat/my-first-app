fff1d09cec68c66894d9bcf4746e940ae5477059
402-04-teeraphat <54187@yupparaj.ac.th>
2026-08-19 18:26:30 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,5 +101,5 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นายธีรภัทร ยามวงศ์ เลขที่ 4 ม.4/"2)
+st.write("นายธีรภัทร ยามวงศ์ เลขที่ 4 ม.4/2")
 
